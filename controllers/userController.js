84705e3c0011b358335506aa79acdca8306e0300
kat <katkatvdd@yahoo.com.ph>
2022-06-20 15:17:32 +0800

    Added login POST and logout GET routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 // Import User model
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const passport = require("passport");
+
+exports.index_get = (req, res, next) => {
+    res.render('index', { title: 'Log In', user: req.user });
+}
+
+exports.signup_get = (req, res, next) => {
+	res.render('signup', { title: 'Sign Up' });
+}
 
 exports.signup_post = (req, res, next) => {
     bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
@@ -14,4 +23,18 @@ exports.signup_post = (req, res, next) => {
             }).save(err => err ? next(err) : res.redirect('/'))
         }
       });
+}
+
+exports.login_post = passport.authenticate("local", {
+    successRedirect: '/',
+    failureRedirect: '/'
+})
+
+exports.logout_get = (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Log In' });
-});
+router.get('/', userController.index_get);
 
-router.get('/sign-up', function(req, res, next) {
-	res.render('signup', { title: 'Sign Up' });
-});
+router.get('/sign-up', userController.signup_get);
 
 router.post('/sign-up', userController.signup_post);
+
+router.post('/log-in', userController.login_post);
+
+router.get('/log-out', userController.logout_get);
 
 module.exports = router;
