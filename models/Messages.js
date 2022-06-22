8c189db62c7c8a2d5661972d2ada9973567aa7c6
kat <katkatvdd@yahoo.com.ph>
2022-06-22 17:21:47 +0800

    Added virtual property to Messages to format the timestamp

--- models/Messages.js
+++ models/Messages.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { format } = require('date-fns')
 const Schema = mongoose.Schema
 
 const MessagesSchema = new Schema({
@@ -12,6 +13,12 @@ const MessagesSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Users'
     }
+})
+
+// Create virtual property
+MessagesSchema.virtual('formatTimestamp').get(function(){
+    // format date
+    return format(this.timestamp, 'h:mma · d MMM yy')
 })
 
 const Messages = mongoose.model('Messages', MessagesSchema)
